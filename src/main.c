0a4b293df37286188e33626d694bc07b7fabbfee
Facundo <facundo.guerrero.pozzi@mi.unc.edu.ar>
2024-11-22 18:32:38 -0300

    command line prompt implementado

--- src/main.c
+++ src/main.c
@@ -1,28 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include <unistd.h>
 #include <cjson/cJSON.h>
+#include <limits.h>
 
 
-int main(void){
 
-    cJSON* root = cJSON_CreateObject();
+void display_prompt() {
+    char cwd[PATH_MAX];
+    char *user = getenv("USER");
+    char hostname[HOST_NAME_MAX];
 
-    cJSON_AddStringToObject(root,"name","Facu");
-    cJSON_AddNumberToObject(root,"age",22);
-    cJSON_AddBoolToObject(root,"is_student",1);
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd() error");
+        return;
+    }
 
-    cJSON* hobbies = cJSON_CreateArray();
+    if (gethostname(hostname, sizeof(hostname)) != 0) {
+        perror("gethostname() error");
+        return;
+    }
 
-    cJSON_AddItemToArray(hobbies, cJSON_CreateString("play Guitar"));
-    cJSON_AddItemToObject(root,"hobbies",hobbies);
+    printf("%s@%s:%s$ ", user, hostname, cwd);
+    fflush(stdout);
+}
 
-    char* json_string = cJSON_Print(root);
-    printf("%s\n", json_string);
+int main() {
+    while (1) {
+        display_prompt();
 
-    cJSON_Delete(root);
-    free(json_string);
+        char command[1024];
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            break; // EOF o error
+        }
 
+        // Aquí manejarás los comandos
+        printf("Comando ingresado: %s", command);
+    }
     return 0;
-
-
 }
