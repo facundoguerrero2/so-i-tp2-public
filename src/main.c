8aae6e492da858c90e58d4a25845f884f84a2546
Facundo <facundo.guerrero.pozzi@mi.unc.edu.ar>
2024-11-23 00:40:30 -0300

    fase 2 completa manejo de comandos internos implementado

--- src/main.c
+++ src/main.c
@@ -3,9 +3,11 @@
 #include <unistd.h>
 #include <cjson/cJSON.h>
 #include <limits.h>
+#include <string.h>
 
 
-
+char *command = NULL;
+FILE *archive = NULL;
 void display_prompt() {
     char cwd[PATH_MAX];
     char *user = getenv("USER");
@@ -25,17 +27,99 @@ void display_prompt() {
     fflush(stdout);
 }
 
-int main() {
-    while (1) {
-        display_prompt();
+void cd_command(char *path) {
+    if (path == NULL || strcmp(path, "") == 0) {
+        path = getenv("HOME");
 
-        char command[1024];
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            break; // EOF o error
+    }else if (strcmp(path, "-") == 0) {
+        path = getenv("OLDPWD");
+    }
+
+    if (chdir(path) != 0) {
+        perror("cd error");
+    } else {
+        setenv("OLDPWD", getenv("PWD"), 1);
+        setenv("PWD", path, 1);
+    }
+}
+
+void echo_command(char *msj) {
+    if(msj[0] == '$'){
+         
+        const char *value = getenv(msj+1);
+        if (value != NULL) {
+            printf("%s\n", value);
+        } else {
+            printf("La variable de entorno %s no está definida.\n", msj+1);
+        }
+    }else{
+       
+        printf("%s ", msj);
+        printf("\n");
+    }
+}
+
+void quit_command(){
+
+    if (command != NULL) {
+        free(command);
+        command = NULL;
+        printf("Memoria dinámica liberada.\n");
+    }
+    if (archive != NULL) {
+        fclose(archive);
+        archive = NULL;
+        printf("Archivo cerrado.\n");
+    }
+}
+
+void command_select(char *input) {
+
+    char *args[128];
+    char *token = strtok(input, " \n");
+    int arg_count = 0;
+
+    while (token != NULL) {
+        args[arg_count++] = token;
+        token = strtok(NULL, " \n");
+    }
+
+    args[arg_count] = NULL;
+
+    if (arg_count == 0) return;
+
+    if (strcmp(args[0], "cd") == 0) {
+        cd_command(args[1]);
+    } else if (strcmp(args[0], "clr") == 0) {
+        system("clear");
+    } else if (strcmp(args[0], "echo") == 0) {
+        echo_command(args[1]);
+    } else if (strcmp(args[0], "quit") == 0) {
+        quit_command();
+        exit(0);
+    } else {
+       // execute_command(args[0], args);
+    }
+}
+
+int main() {
+    
+    size_t len = 0;
+    while (1) {
+        
+        display_prompt();  // Mostrar el prompt
+
+        ssize_t nread = getline(&command, &len, stdin);  // Leer el comando
+
+        if (nread == -1) {
+            break;  // Salir en caso de EOF o error
         }
 
-        // Aquí manejarás los comandos
-        printf("Comando ingresado: %s", command);
+        command_select(command);
     }
+
+    free(command);  // Liberar la memoria asignada para 'command'
     return 0;
 }
+
+
