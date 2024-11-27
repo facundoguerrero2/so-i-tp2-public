98f5482d7fc0b41cc34a65a16f703aaac21e3a4e
Facundo <facundo.guerrero.pozzi@mi.unc.edu.ar>
2024-11-27 05:37:17 -0300

    path bug solve to monitor

--- src/monitor.c
+++ src/monitor.c
@@ -18,7 +18,7 @@ void start_monitor()
     {
         // Proceso hijo: ejecuta el monitor
         execl("/home/fg/Documentos/Facultad/SO/Laboratorios/so-i-24-chp2-facundoguerrero2/build/"
-              "so-i-24-facundoguerrero2/METRICAS",
+              "so-i-24-facundoguerrero2-public/METRICAS",
               "MONITOR", NULL);
         perror("Error al ejecutar el programa de monitoreo");
         exit(EXIT_FAILURE);
